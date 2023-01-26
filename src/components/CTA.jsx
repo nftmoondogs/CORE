1946fb83ca85edf0193ea9d90df0e2c9527b024c
nftmoondogs <nftmoondogs@gmail.com>
2023-01-27 03:12:45 +0530

    form

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -16,7 +16,7 @@ const CardDeal = () => (
 </div>
 
 <div className={` w-full justify-center flex w-[100%] h-[100%] pb-44`}>
-<a href="https://forms.gle/oKqF917BdjrmP7YU8" target="_blank" rel="noreferrer"><button type="button" class="text-yellow-400 hover:text-white border justify-center flex object-center border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Enter Quest</button></a>
+<a href="https://forms.gle/ZN5kBEAhq6JbAKQd7" target="_blank" rel="noreferrer"><button type="button" class="text-yellow-400 hover:text-white border justify-center flex object-center border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Enter Quest</button></a>
 </div>
 </section>
   )
