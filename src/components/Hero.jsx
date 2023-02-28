c65920bb3b858a3799b20464e845b85c2b941bd5
nftmoondogs <nftmoondogs@gmail.com>
2023-02-28 16:05:33 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
         </div>
 
         <div class="flex mt-4 space-x-3 md:mt-6">
-        <a href="https://46bb2cc0.dxapps-2t0.pages.dev/dxsaleview?saleID=25&chain=Core" target="_blank" rel="noreferrer" class=" w-[240px] items-center py-2 px-4 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">WOOF Public Sale</a>
+        <a href="https://dx.app/dxsaleview?saleID=25&chain=Core" target="_blank" rel="noreferrer" class=" w-[240px] items-center py-2 px-4 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">WOOF Public Sale</a>
         </div>
        
 
